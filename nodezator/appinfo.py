b5e904005a200c95dc98931b05ab0e445672c25f
Kennedy Richard <kennedy@kennedyrichard.com>
2022-08-09 15:18:11 -0300

    updated version to 1.2.1

--- nodezator/appinfo.py
+++ nodezator/appinfo.py
@@ -23,7 +23,7 @@ AppVersion = namedtuple(
                'major minor micro release_level'
              )
 
-APP_VERSION = AppVersion(1, 2, 0, 'release_candidate')
+APP_VERSION = AppVersion(1, 2, 1, 'release_candidate')
 
 
 ### titles for the application
