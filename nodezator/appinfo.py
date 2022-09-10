bdaa1ae9b2c41d3c8a613b98189e20cf40629691
Kennedy Richard <kennedy@kennedyrichard.com>
2022-09-10 11:47:12 -0300

    updated version to 1.2.15

--- nodezator/appinfo.py
+++ nodezator/appinfo.py
@@ -20,7 +20,7 @@ from collections import namedtuple
 
 AppVersion = namedtuple("AppVersion", "major minor micro release_level")
 
-APP_VERSION = AppVersion(1, 2, 14, "release_candidate")
+APP_VERSION = AppVersion(1, 2, 15, "release_candidate")
 
 
 ### titles for the application
